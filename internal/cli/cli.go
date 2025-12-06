e3735ada6c12316d3683038589cd1a1705d5a629
allddd <me@allddd.onl>
2025-12-06 19:52:33 +0000

    refactor(cli): replace cobra with standard flag package
    cobra was overkill from the beginning, but was used to avoid reinventing the wheel. when adding more flags it turned out that it still lacks basic functionality like flag grouping. since significant custom code would be needed anyway, switching to standard flag pkg is probbably a good idea

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -1,66 +1,94 @@
 package cli
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"reflect"
+	"strconv"
 
-	"github.com/spf13/cobra"
 	"gitlab.com/allddd/opnsense-filterlog/internal/stream"
 	"gitlab.com/allddd/opnsense-filterlog/internal/tui"
 )
 
 const defaultLogPath = "/var/log/filter/latest.log"
-const helpTemplate = `{{.Short}}
+const usageText = `terminal-based viewer for OPNsense firewall logs
 
 Usage:
-  opnsense-filterlog [flag]... [arg]
+  opnsense-filterlog [flag]... [path]
 
 Arguments:
-  path		  filter log file to analyze, defaults to 'latest.log' if omitted
+  path	filter log file to analyze, defaults to 'latest.log' if omitted
 
 Flags:
-{{.LocalFlags.FlagUsages}}
 `
 
-var version string
-var rootCmd = &cobra.Command{
-	Use:   "opnsense-filterlog",
-	Short: "OPNsense filter log viewer",
-	Args:  cobra.MaximumNArgs(1),
-	Run: func(cli *cobra.Command, args []string) {
+type flags struct {
+	Help    bool `name:"h" usage:"display this help message and exit"`
+	Version bool `name:"V" usage:"display version information and exit"`
+}
 
-		if v, err := cli.Flags().GetBool("version"); err != nil {
-			fmt.Fprintln(os.Stderr, "error: failed to parse 'version' flag: ", err)
-			os.Exit(1)
-		} else if v {
-			fmt.Fprintln(os.Stdout, version)
-			os.Exit(0)
+// flagsDefine defines all flags set in the struct
+func (f *flags) flagsDefine() {
+	sv := reflect.ValueOf(f).Elem()
+	st := sv.Type()
+	for i := 0; i < st.NumField(); i++ {
+		ft := st.Field(i)
+		fv := sv.Field(i)
+		name := ft.Tag.Get("name")
+		usage := ft.Tag.Get("usage")
+		value := ft.Tag.Get("value")
+		switch fv.Kind() {
+		case reflect.Bool:
+			valueBool, _ := strconv.ParseBool(value)
+			flag.BoolVar(fv.Addr().Interface().(*bool), name, valueBool, usage)
+		case reflect.String:
+			flag.StringVar(fv.Addr().Interface().(*string), name, value, usage)
 		}
-
-		if len(args) == 0 {
-			args = []string{defaultLogPath}
-		}
-
-		s, err := stream.NewStream(args[0])
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-
-		if err := tui.Display(s); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-
-	},
+	}
 }
 
 func Execute(v string) {
-	version = v
-	rootCmd.Flags().BoolP("version", "V", false, "display version")
-	rootCmd.SetHelpTemplate(helpTemplate)
+	var f flags
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, usageText)
+		flag.PrintDefaults()
+	}
+	f.flagsDefine()
+	flag.Parse()
+	// check mutually exclusive flags
+	count := 0
+	for _, provided := range []bool{f.Help, f.Version} {
+		if provided {
+			if count++; count > 1 {
+				fmt.Fprintln(os.Stderr, "error: mutually exclusive flags")
+				flag.Usage()
+				os.Exit(1)
+			}
+		}
+	}
+	// -h
+	if f.Help {
+		flag.Usage()
+		os.Exit(0)
+	}
+	// -V
+	if f.Version {
+		fmt.Fprintln(os.Stdout, v)
+		os.Exit(0)
+	}
+	// args
+	args := flag.Args()
+	if len(args) == 0 {
+		args = []string{defaultLogPath}
+	}
 
-	if err := rootCmd.Execute(); err != nil {
+	s, err := stream.NewStream(args[0])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	if err := tui.Display(s); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
